6f049de1cd7c8f7911868495a94876c5019ccc78
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-12 22:25:58 -0400

    solved exercise 3

--- control-flow.py
+++ control-flow.py
@@ -79,12 +79,22 @@ print(f"The dog's age in dog years is {dog_years}")
 #      a: 
 #      b:
 #      c:
+a = input("Enter the length of side a: ")
+b = input("Enter the length of side b: ")
+c = input("Enter the length of side c: ")
 # 2. Write the code that determines if the triangle is:
 #      equilateral - all three sides are equal in length
 #      scalene - all three sides are unequal in length
 #      isosceles - exactly two sides are the same length
+if a == b == c:
+    triangle_type = "equilateral"
+elif a != b != c != a:
+    triangle_type = "scalene"
+else:
+    triangle_type = "isosceles"
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+print(f"A triangle with sides of {a}, {b}, & {c} is a {triangle_type} triangle")
 
 
 
