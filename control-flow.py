8b341834eb07ef59daf8bf5b54cdee3bf8cd094a
Damola Olusakin <olusakinadedamola@gmail.com>
2023-08-12 20:54:43 -0400

    solved exercise 1

--- control-flow.py
+++ control-flow.py
@@ -26,6 +26,16 @@ else:
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
 #      Please enter a word or phrase: 
+phrase = input('Please enter a word or phrase:').lower()
+while True:
+   if phrase == 'quit':
+    break
+   else:
+    print(f'What you entered is {len(phrase)} characters long')
+    continue
+
+    
+  
 # 2. Print the following message:
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
